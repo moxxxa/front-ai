0d634d9d507d78ea46c7223257ad4cf1bfb14462
Moxa <mohaxoy98@gmail.com>
2020-06-07 16:34:28 +0200

    test

--- quasar.conf.js
+++ quasar.conf.js
@@ -46,14 +46,7 @@ module.exports = function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history',
-      env: ctx.dev
-        ? { // so on dev we'll have
-          API: JSON.stringify('http://localhost:8080')
-        }
-        : { // and on build (production):
-          API: JSON.stringify('http://localhost:8080')
-        },
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       distDir: ctx.mode.spa ? 'public' : null,
 
       // Add dependencies for transpiling with Babel (Array of regexes)
